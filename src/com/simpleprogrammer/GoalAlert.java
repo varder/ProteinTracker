636c0e70ce5bf819de3448ec4c86b92994ca8b10
varder <profmaker3@gmai.ocm>
2017-03-23 22:29:34 +0200

    Initial commit

---
+++ src/com/simpleprogrammer/GoalAlert.java
@@ -0,0 +1,28 @@
+package com.simpleprogrammer;
+
+
+public class GoalAlert {
+
+    private int id;
+    private String massege;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMassege() {
+        return massege;
+    }
+
+    public void setMassege(String massege) {
+        this.massege = massege;
+    }
+
+
+
+
+}
---
+++ src/com/simpleprogrammer/Program.java
@@ -0,0 +1,52 @@
+package com.simpleprogrammer;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+public class Program {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		Session session = HibernateUtilities.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		User user = new User();
+		user.setName("Joe");
+		user.addHistory(new UserHistory(new Date(), "Set name to Joe"));
+		user.getProteinData().setGoal(250);
+		user.addHistory(new UserHistory(new Date(), "Set the goal to 250"));
+		
+//		user.getGoalAlerts().add(new GoalAlert("Congratulations!"));
+//		user.getGoalAlerts().add(new GoalAlert("You did it!"));
+		
+		session.save(user);
+				
+		session.getTransaction().commit();
+		
+		session.beginTransaction();
+		
+		User loadedUser = (User) session.load(User.class, 1);
+		System.out.println(loadedUser.getName());
+		System.out.println(loadedUser.getProteinData().getGoal());
+		for(UserHistory history : loadedUser.getHistory())
+		{
+			System.out.println(history.getEntryTime().toString() + " " + history.getEntry());
+		}
+		
+//		loadedUser.getProteinData().setTotal(loadedUser.getProteinData().getTotal() + 50);
+		loadedUser.addHistory(new UserHistory(new Date(), "Added 50 protein"));
+		
+		user.setProteinData(new ProteinData());
+		user.setProteinData(new ProteinData() );
+
+		session.getTransaction().commit();
+		
+		session.close();
+		HibernateUtilities.getSessionFactory().close();
+		System.out.print("complete ");
+
+	}
+
+}
